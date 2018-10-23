08dbb859149bc29c7d66c20abbc4ee5f1cda787a
Mo Iz <mo@mugo.ca>
2018-10-22 18:03:56 -0700

    first commit

---
+++ Clockify.php
@@ -0,0 +1,247 @@
+<?php
+
+class Clockify
+{
+
+    public $apiKey;
+    public $apiEndpoint;
+    public $workspaceId;
+
+    /**
+     * Constructs a Clockify object
+     * @param String $apiKey clockify API key (see https://clockify.github.io/clockify_api_docs/)
+     * @param String $workspace clockify API key (see https://clockify.github.io/clockify_api_docs/#tag-Workspace)
+     * @param String $apiEndpoint you shouldn't have to change this
+     * @throws Exception if $apiKey or $workspace are not provided, or an invalid $apiEndpoint is specified
+     * @returns Clockify
+     */
+    public function __construct( $apiKey, $workspace, $apiEndpoint = "https://api.clockify.me/api/" )
+    {
+
+        if ( !$apiKey ) {
+            throw new Exception( 'You must provide an API key.' );
+        } else {
+            $this->apiKey = $apiKey;
+        }
+
+        if ( !filter_var( $apiEndpoint, FILTER_VALIDATE_URL ) ) {
+            throw new Exception('You must provide a valid API endpoint.');
+        } else {
+            $this->apiEndpoint = $apiEndpoint;
+        }
+
+        try {
+            $clockfiyWorkspaces = $this->apiRequest( 'workspaces/' );
+        } catch( Exception $e ) {
+            return $e;
+        }
+
+        foreach( json_decode( $clockfiyWorkspaces ) as $clockifyWorkspace ) {
+
+            if ( $clockifyWorkspace->name === $workspace ) {
+                $this->workspaceId = $clockifyWorkspace->id;
+            }
+
+        }
+
+        if ( !$this->workspaceId ) {
+            throw new Exception( 'You must provide a valid workspace.' );
+        }
+
+        return $this;
+    }
+
+    public function getReportByDay( $day = "2018-01-31" )
+    {
+        $report = array();
+
+        try {
+            $result = json_decode(
+                $this->apiRequest( 'workspaces/' . $this->workspaceId . '/reports/summary/',
+                    json_encode( array(
+                        "archived" => "Active",
+                        "billable" => "BOTH",
+                        "clientIds" => [],
+                        "description" => "",
+                        "endDate" => $day . "T23:59:59.999Z",
+                        "firstTime" => true,
+                        "includeTimeEntries" => true,
+                        "me" => false,
+                        "name" => "",
+                        "projectIds" => [],
+                        "startDate" => $day . "T00:00:00.000Z",
+                        "tagIds" => [],
+                        "taskIds" => [],
+                        "userGroupIds" => [],
+                        "userIds" => [],
+                        "zoomLevel" => "week"
+                    ) )
+                )
+            );
+        } catch( Exception $e ) {
+            return $e;
+        }
+
+        try {
+            $report[ 'total' ] = new DateInterval( $result->totalTime );
+        } catch ( Exception $e ) {
+            return $e;
+        }
+
+        $report[ 'date' ] = $day;
+        $report[ 'projects' ] = array();
+        $report[ 'total' ] = $report[ 'total' ]->format( '%h hours, %i minutes, %S seconds' );
+
+        foreach ( $result->projectAndTotalTime as $project ) {
+
+            try {
+                $time = new DateInterval( $project-> duration );
+            } catch ( Exception $e ) {
+                return $e;
+            }
+
+            $report[ 'projects' ][ $project->projectName ] = array();
+            $report[ 'projects' ][ $project->projectName ][ 'time' ] = $time->format( '%h hours, %i minutes, %S seconds' );
+            $report[ 'projects' ][ $project->projectName ][ 'entries' ] = array();
+        }
+
+        foreach ( $result->timeEntries as $timeEntry ) {
+            $entry = &$report[ 'projects' ][ $timeEntry->project->name ][ 'entries' ][ $timeEntry->description ];
+
+            if ( !$entry ) {
+                $report[ 'projects' ][ $timeEntry->project->name ][ 'entries' ][ $timeEntry->description ] = array();
+                $entry = &$report[ 'projects' ][ $timeEntry->project->name ][ 'entries' ][ $timeEntry->description ];
+                $entry[ 'intervals' ] = array();
+            }
+
+            array_push( $entry[ 'intervals' ], $timeEntry->timeInterval );
+
+            try {
+                $timeEntryDuration = ClockifyDateInterval::fromDateInterval( new DateInterval( $timeEntry->timeInterval->duration ) );
+            } catch ( Exception $e ) {
+                return $e;
+            }
+
+            if ( $entry[ 'total' ] ) {
+
+                try {
+                    $previousTotalDuration = new DateInterval( $entry[ 'total' ] );
+                } catch ( Exception $e ) {
+                    return $e;
+                }
+
+                $timeEntryDuration->add( $previousTotalDuration );
+                $entry[ 'total' ] = $timeEntryDuration->format( 'PT%hH%iM%sS' );
+                $entry[ 'totalString' ] = $timeEntryDuration->format( '%h hours, %i minutes, %S seconds' );
+            } else {
+                $entry[ 'total' ] = $timeEntryDuration->format('PT%hH%iM%sS');
+                $entry[ 'totalString' ] = $timeEntryDuration->format( '%h hours, %i minutes, %S seconds' );
+            }
+        }
+
+        return $report;
+    }
+
+    public function formatReport( $report )
+    {
+        $result = "---------------------------------------------------------\n" ;
+        $result .= " Report for " . $report['date'] . " (" . $report['total'] . ")\n";
+        $result .= "---------------------------------------------------------\n";
+
+        foreach ($report['projects'] as $key => $project) {
+            $result .="\n";
+            $result .= $key . " (" . $project['time'] . "): \n\n";
+
+            foreach ($project['entries'] as $key2 => $entry) {
+                $result .= "• " . $key2 . " (" . $entry['totalString'] . ")\n";
+            }
+
+        }
+
+        $result .="\n";
+        return $result;
+    }
+
+    public function apiRequest( $apiPath, $payload = false )
+    {
+        $requestHeaders = array(
+            'Content-Type:application/json',
+            'X-Api-Key:' . $this->apiKey
+        );
+
+        if ( $payload ) {
+            $requestHeaders[] = 'Content-Length:' . strlen( $payload );
+        }
+
+        $ch = $this->getCurlObject(
+            $this->apiEndpoint . $apiPath,
+            $requestHeaders,
+            isset( $payload ) ? $payload : false
+        );
+
+        $result = curl_exec( $ch );
+
+        if ( curl_error( $ch ) ) {
+            return curl_error( $ch );
+        } else {
+            return $result;
+        }
+
+    }
+
+    public function getCurlObject( $url, $headers, $payload, $headerFunction = false )
+    {
+        $ch = curl_init();
+        curl_setopt( $ch, CURLOPT_URL, $url );
+
+        if ( $headers ) {
+            curl_setopt( $ch, CURLOPT_HTTPHEADER, $headers );
+        }
+
+        if ( $headerFunction ) {
+            curl_setopt($ch, CURLOPT_HEADERFUNCTION, $headerFunction );
+        }
+
+        curl_setopt( $ch, CURLOPT_FAILONERROR, true );
+        curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+
+        if ( $payload ) {
+            curl_setopt( $ch, CURLOPT_POST, 1 );
+            curl_setopt( $ch, CURLOPT_POSTFIELDS, $payload );
+        } else {
+            curl_setopt( $ch, CURLOPT_HTTPGET, 1 );
+        }
+
+        return $ch;
+    }
+}
+
+/**
+ * Copied almost verbatim from https://gist.github.com/hakre/3142405 (see http://stackoverflow.com/q/11556731/367456)
+ */
+class ClockifyDateInterval extends DateInterval
+{
+    public static function fromDateInterval( DateInterval $from )
+    {
+
+        try {
+            return new ClockifyDateInterval( $from->format( 'P%yY%dDT%hH%iM%sS' ) );
+        } catch ( Exception $e ) {
+            return $e;
+        }
+
+    }
+
+    public function add( DateInterval $interval )
+    {
+
+        foreach ( str_split('ymdhis' ) as $prop ) {
+            $this->$prop += $interval->$prop;
+        }
+
+        $this->i += (int)($this->s / 60);
+        $this->s = $this->s % 60;
+        $this->h += (int)($this->i / 60);
+        $this->i = $this->i % 60;
+    }
+}
---
+++ README.md
@@ -0,0 +1,120 @@
+## Summary
+Rough PHP bindings for the Clockify API. The main reason I built this is because none of Clockify's built-in reports allow time entries with duplicate titles to be aggregated. 
+
+## Installation
+
+Install via Composer
+
+```
+composer install moismailzai/clockify
+```
+
+Or just include ```Clockify.php``` in your project.
+
+## Usage
+
+Use by creating a new Clockify object with your API key and the workspace you want to make reports for.
+
+```
+$clockify = new Clockify( "<< YOUR API KEY >>", "<< YOUR WORKSPACE NAME >>" );
+```
+
+### Making arbitrary API calls
+
+To use this as a PHP wrapper for the API, just pass in the API path to the ```apiRequest``` method:
+
+```
+$clockify->apiRequest( 'workspaces/' . $clockify->workspaceId . '/projects/' )
+```
+
+You can also make POST requests by including a JSON-encoded payload:
+
+```
+$clockify->apiRequest( 'workspaces/' . $clockify->workspaceId . '/reports/summary/',
+    json_encode( array(
+        "archived" => "Active",
+        "billable" => "BOTH",
+        "clientIds" => [],
+        "description" => "",
+        "endDate" => "2018-10-01T23:59:59.999Z",
+        "firstTime" => true,
+        "includeTimeEntries" => true,
+        "me" => false,
+        "name" => "",
+        "projectIds" => [],
+        "startDate" => "2018-10-01T00:00:00.000Z",
+        "tagIds" => [],
+        "taskIds" => [],
+        "userGroupIds" => [],
+        "userIds" => [],
+        "zoomLevel" => "week"
+    ) )
+)
+```
+
+### Generating daily reports
+To generate a daily report, pass in a date to the ```getReportByDay``` method:
+
+```
+$report = $clockify->getReportByDay( '2018-10-01' );
+```
+
+Which will return an object like:
+
+```
+array(3) {
+  ["total"]=>
+  string(31) "0 hours, 12 minutes, 38 seconds"
+  ["date"]=>
+  string(10) "2018-10-01"
+  ["projects"]=>
+  array(1) {
+    ["Project Name"]=>
+    array(2) {
+      ["time"]=>
+      string(31) "3 hours, 38 minutes, 43 seconds"
+      ["entries"]=>
+      array(1) {
+        ["First Entry"]=>
+        array(3) {
+          ["intervals"]=>
+          array(1) {
+            [0]=>
+            object(stdClass)#1 (3) {
+              ["start"]=>
+              string(20) "2018-10-01T16:10:00Z"
+              ["end"]=>
+              string(20) "2018-10-01T16:22:38Z"
+              ["duration"]=>
+              string(8) "PT12M38S"
+            }
+          }
+          ["total"]=>
+          string(9) "PT12M38S"
+          ["totalString"]=>
+          string(31) "0 hours, 12 minutes, 38 seconds"
+        }
+      }
+    }
+  }
+}
+```
+
+And you can format it yourself or pass the result to the built-in formatter:
+
+```
+$clockify->formatReport( $report )
+```
+
+Which will return something like:
+
+```
+
+---------------------------------------------------------
+ Report for 2018-10-01 (0 hours, 12 minutes, 38 seconds)
+---------------------------------------------------------
+
+First Project (0 hours, 12 minutes, 38 seconds): 
+
+• First Entry (0 hours, 12 minutes, 38 seconds)
+```
